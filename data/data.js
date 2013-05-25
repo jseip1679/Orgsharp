f059a42f45c43720206f1f1f5aa1acc9da606c68
Jake Seip <jake.seip@gmail.com>
2013-05-25 16:11:39 -0700

    add bootstrap

--- data/data.js
+++ data/data.js
@@ -4,12 +4,14 @@ var userData = [
     firstname:"Jake",
     lastname: "Seip",
     age: "28",
-    title: "Programmer"
+    title: "Programmer",
+    imgUrl: "http://m3.licdn.com/mpr/pub/image-Z2Nf9njxRb9xp8FVXNle4CxBPRc048MHZ22iRrlxPERpuvUxZ2NiuR8xPvEEuRfduawI/jake-seip.jpg"
   },
   {
     firstname:"Bear",
     lastname: "Dog",
     age: "4",
-    title: "Dog"
+    title: "Dog",
+    imgUrl: " "
   }
 ];
