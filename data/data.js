17bc38c89d18645490031c2523f11a74c281e048
Jake Seip <jake.seip@gmail.com>
2013-05-27 08:43:56 -0700

    tweaks to dummy data

--- data/data.js
+++ data/data.js
@@ -33,8 +33,8 @@ var userData = [
 ];
 
 var hierarchyData = {
-  0:{"root":"true", children: [1,2]},
-  1:{children:[3]},
+  0:{"root":"true", children: [1,3]},
+  1:{children:[2]},
   2:[],
   3:[]
 };
